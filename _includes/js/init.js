ecdb2046686576e49c7fd665c08d2b86552dfad7
Oleg Korsunsky <jatruadmin@gmail.com>
2013-09-17 20:21:38 +0400

    overflow: auto fallback for android 2.x, sniffer updated

--- _includes/js/init.js
+++ _includes/js/init.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	
 	$('.js-gallery').dzGallery();
+	$('pre').dzScrollFallback();
+
+
 	{% if site.dev %}
 		console.log(Pixelizr.run());
 	{% endif %}
--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@
 
 {% include js/gallery.js %}
 
+{% include js/scroll-fallback.js %}
+
 {% include js/init.js %}
 
 {% if site.dev %}{% include js/pixelizr.js %}{% endif %}
