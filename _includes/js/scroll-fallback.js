f85cb776b10892b458a1edadbc5e0be8825f05c4
Oleg Korsunsky <jatruadmin@gmail.com>
2013-09-17 20:21:38 +0400

    overflow: auto fallback for android 2.x, sniffer updated

---
+++ _includes/js/scroll-fallback.js
@@ -0,0 +1,50 @@
+$.fn.dzScrollFallback = function() {
+	if (Sniffer.os.name == 'android' && Sniffer.os.version < 3 && Sniffer.browser.engine == 'webkit') {
+		this.each(function() {
+			var _this = this,
+				last = {},
+				max;
+
+			function setup() {
+				max = {
+					x: _this.scrollWidth - _this.clientWidth,
+					y: _this.scrollHeight - _this.clientHeight
+				};
+			}
+
+			setup();
+
+			_this.addEventListener('touchstart', function(event) {
+
+				last = {
+					x: event.touches[0].clientX,
+					y: event.touches[0].clientY
+				};
+			}, false);
+
+			_this.addEventListener('touchmove', function(event) {
+				var diff = {
+					x: event.touches[0].clientX - last.x,
+					y: event.touches[0].clientY - last.y
+				};
+
+				last = {
+					x: event.touches[0].clientX,
+					y: event.touches[0].clientY
+				};
+
+				_this.scrollLeft -= diff.x;
+
+				if (
+					(_this.scrollTop < max.y && diff.y < 0) ||
+					(_this.scrollTop > 0 && diff.y > 0)) {
+						_this.scrollTop -= diff.y;
+						event.preventDefault();
+				}
+			}, false);
+
+			window.addEventListener('resize', setup, false);
+			window.addEventListener('orientationchange', setup, false);
+		});
+	}
+}
