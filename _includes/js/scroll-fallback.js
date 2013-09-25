55e0d9bef149543ff17f44142f7218e83878f400
Oleg Korsunsky <jatruadmin@gmail.com>
2013-09-25 23:20:46 +0400

    some overflow scrolling improvements

--- _includes/js/scroll-fallback.js
+++ _includes/js/scroll-fallback.js
@@ -1,11 +1,13 @@
 $.fn.dzScrollFallback = function() {
-	if (Sniffer.os.name == 'android' && 
-		((Sniffer.os.version < 3 && Sniffer.browser.engine == 'webkit') ||
-		(Sniffer.browser.engine == 'presto' && Sniffer.browser.name == 'opera'))) {
+	if ((Sniffer.os.name == 'android' && 
+			((Sniffer.os.version < 3 && Sniffer.browser.engine == 'webkit') ||
+			(Sniffer.browser.engine == 'presto' && Sniffer.browser.name == 'opera'))) ||
+		(Sniffer.os.name == 'ios' && Sniffer.os.version <= 5 && Sniffer.browser.engine == 'webkit')) {
 		this.each(function() {
 			var _this = this,
 				last = {},
-				max;
+				max,
+				isVerticalScrolling;
 
 			function setup() {
 				max = {
@@ -17,11 +19,12 @@ $.fn.dzScrollFallback = function() {
 			setup();
 
 			_this.addEventListener('touchstart', function(event) {
-
 				last = {
 					x: event.touches[0].clientX,
 					y: event.touches[0].clientY
 				};
+
+				isVerticalScrolling = undefined;
 			}, false);
 
 			_this.addEventListener('touchmove', function(event) {
@@ -35,13 +38,25 @@ $.fn.dzScrollFallback = function() {
 					y: event.touches[0].clientY
 				};
 
-				_this.scrollLeft -= diff.x;
+				if (isVerticalScrolling === undefined) {
+					isVerticalScrolling = Math.abs(diff.x) < Math.abs(diff.y);
+				}
 
-				if (
-					(_this.scrollTop < max.y && diff.y < 0) ||
-					(_this.scrollTop > 0 && diff.y > 0)) {
-						_this.scrollTop -= diff.y;
-						event.preventDefault();
+				if (isVerticalScrolling) {
+					if (
+						(_this.scrollTop < max.y && diff.y < 0) ||
+						(_this.scrollTop > 0 && diff.y > 0)) {
+							_this.scrollTop -= diff.y;
+							event.preventDefault();
+					}
+				}
+				else {
+					if (
+						(_this.scrollLeft < max.x && diff.x < 0) ||
+						(_this.scrollLeft > 0 && diff.x > 0)) {
+							_this.scrollLeft -= diff.x;
+							event.preventDefault();
+					}
 				}
 			}, false);
 
