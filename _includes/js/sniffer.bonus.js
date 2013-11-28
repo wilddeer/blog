46a24360f6c71dd01fad42e9d79a0b6afb803bc2
Oleg Korsunsky <jatruadmin@gmail.com>
2013-11-28 16:09:07 +0400

    dirty hack made dirtier

--- _includes/js/sniffer.bonus.js
+++ _includes/js/sniffer.bonus.js
@@ -8,7 +8,7 @@
 		tag.className = tag.className.replace('fontface','no-fontface');
 		Modernizr.fontface = false;
 	}
-	else if (Sniffer.os.name != 'kindle' && navigator.userAgent.indexOf('armv7l') != -1 && navigator.userAgent.indexOf('X11') != -1) {
+	else if (Sniffer.os.name != 'kindle' && Sniffer.browser.engine == 'webkit' && navigator.userAgent.indexOf('armv7l') != -1 && navigator.userAgent.indexOf('X11') != -1) {
 		tag.className += ' kindle bw';
 		Sniffer.os.name = 'kindle';
 		Sniffer.os.version = 4;
