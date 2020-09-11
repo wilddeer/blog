59156edf8edd0e7c8038829314ea97ab585fe954
Oleg Korsunsky <wd@dizaina.net>
2020-09-11 18:09:10 +0300

    fixes and improvements

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,19 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const yaml = require('js-yaml');
 
 module.exports = config => {
+    // Plugins
     config.addPlugin(syntaxHighlight);
 
     config.setDataDeepMerge(true);
 
     config.addDataExtension('yaml', contents => yaml.safeLoad(contents));
 
+    config.addFilter('formatDate', (dateString, lang) => {
+        const date = new Date(dateString);
+        return +date;
+    });
+
+    // Passthrough
     config.addPassthroughCopy('posts/**/*.jpg');
     config.addPassthroughCopy('posts/**/*.png');
     config.addPassthroughCopy('posts/**/*.webp');
